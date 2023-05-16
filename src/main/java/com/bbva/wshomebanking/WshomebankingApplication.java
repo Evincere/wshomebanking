2e6c534e942592d688d9f5d2a80534f431f2cfd8
Tobias Olea <tobiolea97@gmail.com>
2023-05-16 15:07:05 -0300

    Versión inicial

---
+++ src/main/java/com/bbva/wshomebanking/WshomebankingApplication.java
@@ -0,0 +1,13 @@
+package com.bbva.wshomebanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WshomebankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WshomebankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bbva/wshomebanking/WshomebankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbva.wshomebanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WshomebankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
